6a06d1da36ab67ec87014d44bc15a18ec1051c11
muzakparov <muzakparov@gmail.com>
2017-11-14 20:21:10 +0200

    add room

--- app.js
+++ app.js
@@ -6,22 +6,16 @@ app.get('/', function (req, res) {
     res.sendFile("index.html", { "root": __dirname });
 });
 
-var clients = 0;
-io.on('connection', function(socket) {
-   clients++;
-   socket.emit('newclientconnect',{ description: 'Hey, welcome!'});
-   socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
-   socket.on('disconnect', function () {
-      clients--;
-      socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
-   });
-});
+var roomno = 1;
+io.on('connection', function (socket) {
 
-var nsp = io.of('/my-namespace');
-nsp.on('connection', function(socket) {
-   console.log('someone connected');
-   nsp.emit('hi', 'Hello everyone!');
-});
+    //Increase roomno 2 clients are present in a room.
+    if (io.nsps['/'].adapter.rooms["room-" + roomno] && io.nsps['/'].adapter.rooms["room-" + roomno].length > 1) roomno++;
+    socket.join("room-" + roomno);
+
+    //Send this event to everyone in the room.
+    io.sockets.in("room-" + roomno).emit('connectToRoom', "You are in room no. " + roomno);
+})
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
