a7f782b7ab7fd3e5c6b8d566f9be3fa6f05e5a6f
muzakparov <muzakparov@gmail.com>
2017-11-14 19:34:41 +0200

    broadcast

--- app.js
+++ app.js
@@ -7,31 +7,14 @@ app.get('/', function (req, res) {
 });
 
 var clients = 0;
-io.on('connection', function (socket) {
-    console.log('A user connected');
-
-    //Send a message after a timeout of 4seconds
-    setTimeout(function () {
-        socket.send('Sent a message 4seconds after connection!');
-    }, 4000);
-
-    setTimeout(function () {
-        //Sending an object when emmiting an event
-        socket.emit('testerEvent', { description: 'A custom event named testerEvent!' });
-    }, 5000);
-
-    socket.on('clientEvent', function (data) {
-        console.log(data);
-    });
-
-
-    clients++;
-    io.sockets.emit('broadcast', { description: clients + ' clients connected!' });
-    socket.on('disconnect', function () {
-        clients--;
-        io.sockets.emit('broadcast', { description: clients + ' clients connected!' });
-        console.log('A user disconnected');
-    });
+io.on('connection', function(socket) {
+   clients++;
+   socket.emit('newclientconnect',{ description: 'Hey, welcome!'});
+   socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
+   socket.on('disconnect', function () {
+      clients--;
+      socket.broadcast.emit('newclientconnect',{ description: clients + ' clients connected!'})
+   });
 });
 
 http.listen(3000, function () {
